ce7463b1f1b7d0dc98f9193801afbf35c37035a5
DaimerlisEscalona <daimerlisescalona@gmail.com>
2023-01-21 10:29:11 -0300

    agregando limit, page

--- src/Services/consultas.js
+++ src/Services/consultas.js
@@ -1,9 +1,13 @@
-const pool = require("pg")
+const format = require('pg-format');
+const pool = require('../DataBase/db')
 
- const obtenerJoyas = async ({ limits = 10}) =>{
-     let consulta = "SELECT * FROM joyas LIMIT $1"
-     const { rows: joyas } = await pool.query(consulta, [limits])
-     return joyas
+const obtenerJoyas = async ({ limits = 3, page = 1, order_by = "stock_ASC"}) => {
+
+    let offset =  (page - 1) * limits;
+    const [Stock] = order_by.split("_");
+    const consulta = format ('SELECT * FROM inventario ORDER BY %s LIMIT %s OFFSET %s', limits, offset,Stock)
+    const { rows: joyas } = await pool.query(consulta);
+    return joyas
 }
 
 module.exports = { obtenerJoyas }
