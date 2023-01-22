5639e0cad4b719d13f82b405e5a6e49ab9c28ad2
DaimerlisEscalona <daimerlisescalona@gmail.com>
2023-01-22 16:45:29 -0300

    agregando middlewares

--- src/Routes/indexRoutes.js
+++ src/Routes/indexRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+const reportarConsulta = require("../middeleware/reportarConsulta");
 const indexControllers = require("../Controllers/indexControllers");
 
-router.get("/joyas", indexControllers.getJoyas)
-router.get("/joyas/filtros", indexControllers.getFiltros)
-router.get("/joyas/joya/:id", indexControllers.getJoya);
+router.get("/joyas", reportarConsulta, indexControllers.getJoyas)
+router.get("/joyas/filtros", reportarConsulta, indexControllers.getFiltros)
+router.get("/joyas/joya/:id", reportarConsulta, indexControllers.getJoya);
 
 module.exports = router;
